56ea5b61d2c3e7720392de0c5163b209fdde3f49
Oliver Critchfield <oliver.critchfield@gmail.com>
2018-07-03 17:48:33 +0100

    Add live when heart collected

--- js/app.js
+++ js/app.js
@@ -1,41 +1,46 @@
 // Enemies our player must avoid
 class Enemy {
   constructor() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a provided helper to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // The starting position for the enemy
     this.x = 1;
     this.y = Math.floor(Math.random()*3)*83 + 60;
+
+    // The starting speed for the enemy, selected randomly
     this.speed = Math.floor(Math.random()*3)*200 + 100;
-  }
+  };
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
   update(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // Multiply any movement by the dt parameter to ensure
+    // the game runs at the same speed for all computers.
     this.x += this.speed * dt;
+
+    // If the enemy exits screen right, randomly assign a
+    // new y-coordinate and speed for a new enemy
     if (this.x >= 505) {
       this.x = -100;
       this.y = Math.floor(Math.random()*3)*83 + 60;
       this.speed = Math.floor(Math.random()*3)*200 + 100;
     };
-  }
+  };
 
   // Draw the enemy on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
+  };
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-let scoreHTML = document.querySelector('.current-score')
+// The player class will alter the score displayed in the HTML
+let scoreHTML = document.querySelector('.current-score');
+
+// Player class, used to inititiate new players
+// This class has methods update(), render() and
+// handleInput()
 class Player {
   constructor() {
     // The image/ sprite for the player. This uses
@@ -43,11 +48,13 @@ class Player {
     this.sprite = 'images/char-boy.png';
     this.x = 203;
     this.y = 375;
-  }
+  };
 
+  // Update the player's situation
   update() {
-    // If the player reaches the water then add 1 to the number
-    // of wins and restart the game
+    // If the player reaches the water then add 1 to the
+    // number of wins, add 20 to the score and move player
+    // to original position
     if (this.y < 43) {
       this.x = 203;
       this.y = 375;
@@ -56,10 +63,11 @@ class Player {
       scoreHTML.textContent = `Score: ${score}`;
     };
 
-    // If the player collides with an enemy, then restart the
-    // game and reset the score
+    // If the player collides with an enemy, then remove a
+    // life and move the player back to the start. If there
+    // are no lives left, display an end of game modal.
     allEnemies.forEach((enemy) => {
-      if (this.y === (enemy.y - 17) && this.x > enemy.x - 25 && this.x < enemy.x + 25) {
+      if (this.y === (enemy.y - 17) && this.x > enemy.x - 35 && this.x < enemy.x + 35) {
         this.x = 203;
         this.y = 375;
         lives -= 1;
@@ -73,28 +81,32 @@ class Player {
 
     // If the player lands on the same square as the gem
     // then collect the gem, add 10 to the score and set a
-    // new gem down
+    // new gem down. If the player lands on a heard, add a
+    // life if there are fewer than three lives currently.
     if (this.x === collectable.x && this.y === collectable.y - 17) {
-      if (collectable.sprite === 'images/Heart.png') {
+      if (collectable.sprite === 'images/Heart.png' && lives != 3) {
         lives += 1;
+        addHeart();
       } else {
         score += 10;
+        scoreHTML.textContent = `Score: ${score}`;
         gems += 1;
       };
 
-      scoreHTML.textContent = `Score: ${score}`;
-
+      // Set new gem down
       collectable.sprite = collectables[ Math.floor(Math.random()*3)];
       collectable.x = Math.floor(Math.random()*5)*101 + 1;
       collectable.y = Math.floor(Math.random()*3)*83 + 60;
       collectable.render();
     };
-  }
+  };
 
+  // Draw the player on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
+  };
 
+  // Dictate how the player moves based on keyboard input
   handleInput(keyInput) {
     switch (keyInput) {
       case 'left':
@@ -117,17 +129,20 @@ class Player {
           this.y += 83;
         };
         break;
-    }
-  }
+    };
+  };
 };
 
+// Collectables class, used to initiave new gems or
+// lives. This class has method render()
 class Collectable {
   constructor() {
     this.sprite = collectables[Math.floor(Math.random()*4)];
     this.x = Math.floor(Math.random()*5)*101 + 1;
     this.y = Math.floor(Math.random()*3)*83 + 60;
-  }
+  };
 
+  // Draw the gem or heart on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
@@ -136,9 +151,17 @@ class Collectable {
 const heartDisplay = document.querySelector('.hearts');
 // A function to remove a heart when a life is lost
 function removeHeart() {
-  let hearts = heartDisplay.querySelectorAll('.fa-heart');
-  hearts[hearts.length - 1].classList.remove('fa-heart');
-  hearts[hearts.length - 1].classList.add('fa-heart-o');
+  let heartsRemaining = heartDisplay.querySelectorAll('.fa-heart');
+  heartsRemaining[heartsRemaining.length - 1].classList.remove('fa-heart');
+  heartsRemaining[heartsRemaining.length - 1].classList.add('fa-heart-o');
+};
+
+// A function to add a heart when a heart is collected
+function addHeart() {
+  let heartsRemaining = heartDisplay.querySelectorAll('.fa-heart');
+  let heartsAll = heartDisplay.querySelectorAll('.fa');
+  heartsAll[heartsRemaining.length].classList.remove('fa-heart-o');
+  heartsAll[heartsRemaining.length].classList.add('fa-heart');
 };
 
 // If the new game button is pressed then restart the game
@@ -147,6 +170,9 @@ newGameButton.addEventListener('click', function(evt) {
   resetCounters();
 });
 
+// A function to be called when a game restarts, which
+// resets wins, score and lives counters and the necessary
+// displays
 function resetCounters() {
   // Move player back to start
   player.x = 203;
@@ -159,7 +185,7 @@ function resetCounters() {
   lives = 3;
   scoreHTML.textContent = `Score: ${score}`;
 
-  // Reset star rating
+  // Reset star rating display
   let hearts = heartDisplay.querySelectorAll('.fa');
   hearts.forEach(function(heart) {
     heart.classList.add('fa-heart');
